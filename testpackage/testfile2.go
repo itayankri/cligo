22bcb0e5527b514c4f0f1ed72bc4b5051f0ef1b1
Itay Ankri <itayan116@gmail.com>
2019-12-28 12:44:10 +0200

    added testfile2

---
+++ testpackage/testfile2.go
@@ -0,0 +1,33 @@
+package testpackage
+
+import "fmt"
+
+func CliAdd(x, y float64) {
+	fmt.Println(x + y)
+}
+
+func CliAddMany(nums ...float64) {
+	var sum float64
+
+	for _, num := range nums {
+		sum += num
+	}
+
+	fmt.Println(sum)
+}
+
+func CliSub(x, y float64) {
+	fmt.Println(x - y)
+}
+
+func CliMul(x, y float64) {
+	fmt.Println(x * y)
+}
+
+func CliDiv(x, y float64) {
+	if y == 0 {
+		fmt.Println("cannot divide number by 0")
+	} else {
+		fmt.Println(x / y)
+	}
+}
